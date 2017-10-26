0c12c854a1a449600af8b16483d6de23e05ba4fa
David Fekke <david@fekke.com>
2017-10-26 09:04:24 -0400

    Adding initial commit

---
+++ Contents.swift
@@ -0,0 +1,28 @@
+//: Playground - noun: a place where people can play
+
+import Cocoa
+
+// # This is an playground with example from the JaxARCSIG presentation on Swift 4
+
+let group = "JaxARCSIG"
+
+var str = "Hello, \(group)"
+
+print(str)
+
+// Use var for mutable values
+var name = "David"
+var age = 29
+
+// change values
+name = "Dave"
+age = 30
+
+// Use let for immutable values
+let myName = "Bob"
+let myAge = 41
+
+// trying to change the assignment
+// myName = "Bobby" // will return an error
+
+
